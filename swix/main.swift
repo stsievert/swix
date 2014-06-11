7868723d3b0008083a0ae5fc82d4798e104cbb73
scottsievert <sieve121@umn.edu>
2014-06-11 10:23:46 -0500

    Initial Commit

---
+++ swix/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swix
+//
+//  Created by Scott Sievert on 6/11/14.
+//  Copyright (c) 2014 com.scott. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
