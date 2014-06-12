a902971720704ca43eeaf9937504605968a11c4d
scottsievert <sieve121@umn.edu>
2014-06-11 19:39:53 -0500

    Before from array to matrix

---
+++ swix/array.swift
@@ -0,0 +1,18 @@
+//
+//  array.swift
+//  swix
+//
+//  Created by Scott Sievert on 6/11/14.
+//  Copyright (c) 2014 com.scott. All rights reserved.
+//
+
+import Foundation
+typealias matrix = Array<Double>
+
+
+
+func onesMatrix(N: Int){
+    var x = Array(count: N, repeatedValue: 1.0)
+    var y = x
+}
+
