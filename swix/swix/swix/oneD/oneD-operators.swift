9d38396cac22c98f7fa85e8c8b1b0e2720d1e03f
scottsievert <sieve121@umn.edu>
2014-08-04 13:31:29 -0500

    moving operators to b5

--- swix/swix/swix/oneD/oneD-operators.swift
+++ swix/swix/swix/oneD/oneD-operators.swift
@@ -9,7 +9,7 @@
 import Foundation
 import Accelerate
 
-func make_operator(lhs:ndarray, operator:String, rhs:ndarray) -> ndarray{
+func make_operator(lhs:ndarray, operation:String, rhs:ndarray) -> ndarray{
     assert(lhs.n == rhs.n, "Sizes must match!")
     var array = zeros(lhs.n) // lhs[i], rhs[i]
     var arg_b = zeros(lhs.n)
@@ -19,69 +19,69 @@ func make_operator(lhs:ndarray, operator:String, rhs:ndarray) -> ndarray{
     // [1]:https://github.com/haginile/SwiftAccelerate
     var result = lhs.copy()
     var N = lhs.n
-    if operator=="+"
+    if operation=="+"
         {cblas_daxpy(N.cint, 1.0.cdouble, !rhs, 1.cint, !result, 1.cint);}
-    else if operator=="-"
+    else if operation=="-"
         {cblas_daxpy(N.cint, -1.0.cdouble, !rhs, 1.cint, !result, 1.cint);}
-    else if operator=="*"
+    else if operation=="*"
         {vDSP_vmulD(!lhs, 1, !rhs, 1, !result, 1, vDSP_Length(lhs.grid.count))}
-    else if operator=="/"
+    else if operation=="/"
         {vDSP_vdivD(!rhs, 1, !lhs, 1, !result, 1, vDSP_Length(lhs.grid.count))}
-    else if operator=="<" || operator==">" || operator==">=" || operator=="<=" || operator=="==" || operator=="!=="{
+    else if operation=="<" || operation==">" || operation==">=" || operation=="<=" || operation=="==" || operation=="!=="{
         result = zeros(lhs.n)
-        CVWrapper.compare(!lhs, with: !rhs, using: operator.nsstring, into: !result, ofLength: lhs.n.cint)
+        CVWrapper.compare(!lhs, with: !rhs, using: operation.nsstring, into: !result, ofLength: lhs.n.cint)
         // since opencv uses images which use 8-bit values
         result /= 255
     }
-    else {assert(false, "Operator not recongized!")}
+    else {assert(false, "operation not recongized!")}
     return result
 }
-func make_operator(lhs:ndarray, operator:String, rhs:Double) -> ndarray{
+func make_operator(lhs:ndarray, operation:String, rhs:Double) -> ndarray{
     var array = zeros(lhs.n)
     var right = [rhs]
-    if operator == "%"
+    if operation == "%"
         // unoptimized. for loop in c
         {mod_objc(!lhs, rhs, !array, lhs.n.cint);
-    } else if operator == "*"
+    } else if operation == "*"
         {mul_scalar_objc(!lhs, rhs.cdouble, !array, lhs.n.cint)}
-    else if operator == "+"
+    else if operation == "+"
         {vDSP_vsaddD(!lhs, 1, &right, !array, 1, vDSP_Length(lhs.grid.count))}
-    else if operator=="/"
+    else if operation=="/"
         {vDSP_vsdivD(!lhs, 1, &right, !array, 1, vDSP_Length(lhs.grid.count))}
-    else if operator=="-"
-        {array = make_operator(lhs, "-", ones(lhs.n)*rhs)}
-    else if operator=="<" || operator==">" || operator=="<=" || operator==">="{
-        CVWrapper.compare(!lhs, withDouble:rhs.cdouble, using:operator.nsstring, into:!array, ofLength:lhs.n.cint)
+    else if operation=="-"
+        {array = make_operation(lhs, "-", ones(lhs.n)*rhs)}
+    else if operation=="<" || operation==">" || operation=="<=" || operation==">="{
+        CVWrapper.compare(!lhs, withDouble:rhs.cdouble, using:operation.nsstring, into:!array, ofLength:lhs.n.cint)
         array /= 255
     }
-    else {assert(false, "Operator not recongnized! Error with the speedup?")}
+    else {assert(false, "operation not recongnized! Error with the speedup?")}
     return array
 }
-func make_operator(lhs:Double, operator:String, rhs:ndarray) -> ndarray{
+func make_operator(lhs:Double, operation:String, rhs:ndarray) -> ndarray{
     var array = zeros(rhs.n) // lhs[i], rhs[i]
     var l = ones(rhs.n) * lhs
-    if operator == "*"
+    if operation == "*"
         {array = make_operator(rhs, "*", lhs)}
-    else if operator == "+"{
+    else if operation == "+"{
         array = make_operator(rhs, "+", lhs)}
-    else if operator=="-"
+    else if operation=="-"
         {array = -1 * make_operator(rhs, "-", lhs)}
-    else if operator=="/"{
+    else if operation=="/"{
         array = make_operator(l, "/", rhs)}
-    else if operator=="<"{
+    else if operation=="<"{
         array = make_operator(rhs, ">", lhs)}
-    else if operator==">"{
+    else if operation==">"{
         array = make_operator(rhs, "<", lhs)}
-    else if operator=="<="{
+    else if operation=="<="{
         array = make_operator(rhs, ">=", lhs)}
-    else if operator==">="{
+    else if operation==">="{
         array = make_operator(rhs, "<=", lhs)}
     else {assert(false, "Operator not reconginzed")}
     return array
 }
 
 // EQUALITY
-operator infix ~== {associativity none precedence 140}
+infix operator ~== {associativity none precedence 140}
 func ~== (lhs: ndarray, rhs: ndarray) -> Bool{
     assert(lhs.n == rhs.n, "`~==` only works on arrays of equal size")
     return max(abs(lhs - rhs)) > 1e-6 ? false : true;
@@ -92,25 +92,25 @@ func !== (lhs: ndarray, rhs: ndarray) -> ndarray{
     return make_operator(lhs, "!==", rhs)}
 
 // NICE ARITHMETIC
-@assignment func += (inout x: ndarray, right: Double){
+func += (inout x: ndarray, right: Double){
     x = x + right}
-@assignment func *= (inout x: ndarray, right: Double){
+func *= (inout x: ndarray, right: Double){
     x = x * right}
-@assignment func -= (inout x: ndarray, right: Double){
+func -= (inout x: ndarray, right: Double){
     x = x - right}
-@assignment func /= (inout x: ndarray, right: Double){
+func /= (inout x: ndarray, right: Double){
     x = x / right}
 
 // MOD
-operator infix % {associativity none precedence 140}
+infix operator % {associativity none precedence 140}
 func % (lhs: ndarray, rhs: Double) -> ndarray{
     return make_operator(lhs, "%", rhs)}
 // POW
-operator infix ^ {associativity none precedence 140}
+infix operator ^ {associativity none precedence 140}
 func ^ (lhs: ndarray, rhs: Double) -> ndarray{
     return pow(lhs, rhs)}
 // PLUS
-operator infix + {associativity none precedence 140}
+infix operator + {associativity none precedence 140}
 func + (lhs: ndarray, rhs: ndarray) -> ndarray{
     return make_operator(lhs, "+", rhs)}
 func + (lhs: Double, rhs: ndarray) -> ndarray{
@@ -118,7 +118,7 @@ func + (lhs: Double, rhs: ndarray) -> ndarray{
 func + (lhs: ndarray, rhs: Double) -> ndarray{
     return make_operator(lhs, "+", rhs)}
 // MINUS
-operator infix - {associativity none precedence 140}
+infix operator - {associativity none precedence 140}
 func - (lhs: ndarray, rhs: ndarray) -> ndarray{
     return make_operator(lhs, "-", rhs)}
 func - (lhs: Double, rhs: ndarray) -> ndarray{
@@ -126,7 +126,7 @@ func - (lhs: Double, rhs: ndarray) -> ndarray{
 func - (lhs: ndarray, rhs: Double) -> ndarray{
     return make_operator(lhs, "-", rhs)}
 // TIMES
-operator infix * {associativity none precedence 140}
+infix operator * {associativity none precedence 140}
 func * (lhs: ndarray, rhs: ndarray) -> ndarray{
     return make_operator(lhs, "*", rhs)}
 func * (lhs: Double, rhs: ndarray) -> ndarray{
@@ -134,7 +134,7 @@ func * (lhs: Double, rhs: ndarray) -> ndarray{
 func * (lhs: ndarray, rhs: Double) -> ndarray{
     return make_operator(lhs, "*", rhs)}
 // DIVIDE
-operator infix / {associativity none precedence 140}
+infix operator / {associativity none precedence 140}
 func / (lhs: ndarray, rhs: ndarray) -> ndarray{
     return make_operator(lhs, "/", rhs)
     }
@@ -143,7 +143,7 @@ func / (lhs: Double, rhs: ndarray) -> ndarray{
 func / (lhs: ndarray, rhs: Double) -> ndarray{
     return make_operator(lhs, "/", rhs)}
 // LESS THAN
-operator infix < {associativity none precedence 140}
+infix operator < {associativity none precedence 140}
 func < (lhs: ndarray, rhs: Double) -> ndarray{
     return make_operator(lhs, "<", rhs)}
 func < (lhs: ndarray, rhs: ndarray) -> ndarray{
@@ -151,7 +151,7 @@ func < (lhs: ndarray, rhs: ndarray) -> ndarray{
 func < (lhs: Double, rhs: ndarray) -> ndarray{
     return make_operator(lhs, "<", rhs)}
 // GREATER THAN
-operator infix > {associativity none precedence 140}
+infix operator > {associativity none precedence 140}
 func > (lhs: ndarray, rhs: Double) -> ndarray{
     return make_operator(lhs, ">", rhs)}
 func > (lhs: ndarray, rhs: ndarray) -> ndarray{
@@ -159,7 +159,7 @@ func > (lhs: ndarray, rhs: ndarray) -> ndarray{
 func > (lhs: Double, rhs: ndarray) -> ndarray{
     return make_operator(lhs, ">", rhs)}
 // GREATER THAN OR EQUAL
-operator infix >= {associativity none precedence 140}
+infix operator >= {associativity none precedence 140}
 func >= (lhs: ndarray, rhs: Double) -> ndarray{
     return make_operator(lhs, ">=", rhs)}
 func >= (lhs: ndarray, rhs: ndarray) -> ndarray{
@@ -167,7 +167,7 @@ func >= (lhs: ndarray, rhs: ndarray) -> ndarray{
 func >= (lhs: Double, rhs: ndarray) -> ndarray{
     return make_operator(lhs, ">=", rhs)}
 // LESS THAN OR EQUAL
-operator infix <= {associativity none precedence 140}
+infix operator <= {associativity none precedence 140}
 func <= (lhs: ndarray, rhs: Double) -> ndarray{
     return make_operator(lhs, "<=", rhs)}
 func <= (lhs: ndarray, rhs: ndarray) -> ndarray{
--- swix/swix/swix/twoD/twoD-operators.swift
+++ swix/swix/swix/twoD/twoD-operators.swift
@@ -9,7 +9,7 @@
 import Foundation
 import Accelerate
 
-func make_operator(lhs: matrix, operator: String, rhs: matrix)->matrix{
+func make_operator(lhs: matrix, operation: String, rhs: matrix)->matrix{
     assert(lhs.shape.0 == rhs.shape.0, "Sizes must match!")
     assert(lhs.shape.1 == rhs.shape.1, "Sizes must match!")
     
@@ -17,74 +17,74 @@ func make_operator(lhs: matrix, operator: String, rhs: matrix)->matrix{
     var lhsM = lhs.flat
     var rhsM = rhs.flat
     var resM:ndarray = zeros_like(lhsM) // flat ndarray
-    if operator=="+" {resM = lhsM + rhsM}
-    else if operator=="-" {resM = lhsM - rhsM}
-    else if operator=="*" {resM = lhsM * rhsM}
-    else if operator=="/" {resM = lhsM / rhsM}
-    else if operator=="<" {resM = lhsM < rhsM}
-    else if operator==">" {resM = lhsM > rhsM}
-    else if operator==">=" {resM = lhsM >= rhsM}
-    else if operator=="<=" {resM = lhsM <= rhsM}
+    if operation=="+" {resM = lhsM + rhsM}
+    else if operation=="-" {resM = lhsM - rhsM}
+    else if operation=="*" {resM = lhsM * rhsM}
+    else if operation=="/" {resM = lhsM / rhsM}
+    else if operation=="<" {resM = lhsM < rhsM}
+    else if operation==">" {resM = lhsM > rhsM}
+    else if operation==">=" {resM = lhsM >= rhsM}
+    else if operation=="<=" {resM = lhsM <= rhsM}
     result.flat.grid = resM.grid
     return result
 }
-func make_operator(lhs: matrix, operator: String, rhs: Double)->matrix{
+func make_operator(lhs: matrix, operation: String, rhs: Double)->matrix{
     var result = zeros_like(lhs) // real result
 //    var lhsM = asmatrix(lhs.grid) // flat
     var lhsM = lhs.flat
     var resM:ndarray = zeros_like(lhsM) // flat matrix
-    if operator=="+" {resM = lhsM + rhs}
-    else if operator=="-" {resM = lhsM - rhs}
-    else if operator=="*" {resM = lhsM * rhs}
-    else if operator=="/" {resM = lhsM / rhs}
-    else if operator=="<" {resM = lhsM < rhs}
-    else if operator==">" {resM = lhsM > rhs}
-    else if operator==">=" {resM = lhsM >= rhs}
-    else if operator=="<=" {resM = lhsM <= rhs}
+    if operation=="+" {resM = lhsM + rhs}
+    else if operation=="-" {resM = lhsM - rhs}
+    else if operation=="*" {resM = lhsM * rhs}
+    else if operation=="/" {resM = lhsM / rhs}
+    else if operation=="<" {resM = lhsM < rhs}
+    else if operation==">" {resM = lhsM > rhs}
+    else if operation==">=" {resM = lhsM >= rhs}
+    else if operation=="<=" {resM = lhsM <= rhs}
     result.flat.grid = resM.grid
     return result
 }
-func make_operator(lhs: Double, operator: String, rhs: matrix)->matrix{
+func make_operator(lhs: Double, operation: String, rhs: matrix)->matrix{
     var result = zeros_like(rhs) // real result
 //    var rhsM = asmatrix(rhs.grid) // flat
     var rhsM = rhs.flat
     var resM:ndarray = zeros_like(rhsM) // flat matrix
-    if operator=="+" {resM = lhs + rhsM}
-    else if operator=="-" {resM = lhs - rhsM}
-    else if operator=="*" {resM = lhs * rhsM}
-    else if operator=="/" {resM = lhs / rhsM}
-    else if operator=="<" {resM = lhs < rhsM}
-    else if operator==">" {resM = lhs > rhsM}
-    else if operator==">=" {resM = lhs >= rhsM}
-    else if operator=="<=" {resM = lhs <= rhsM}
+    if operation=="+" {resM = lhs + rhsM}
+    else if operation=="-" {resM = lhs - rhsM}
+    else if operation=="*" {resM = lhs * rhsM}
+    else if operation=="/" {resM = lhs / rhsM}
+    else if operation=="<" {resM = lhs < rhsM}
+    else if operation==">" {resM = lhs > rhsM}
+    else if operation==">=" {resM = lhs >= rhsM}
+    else if operation=="<=" {resM = lhs <= rhsM}
     result.flat.grid = resM.grid
     return result
 }
 
 // DOT PRODUCT
-operator infix *! {associativity none precedence 140}
+infix operator *! {associativity none precedence 140}
 func *! (lhs: matrix, rhs: matrix) -> matrix{
     return dot(lhs, rhs)}
 // SOLVE
-operator infix !/ {associativity none precedence 140}
+infix operator !/ {associativity none precedence 140}
 func !/ (lhs: matrix, rhs: ndarray) -> ndarray{
     return solve(lhs, rhs)}
 // EQUALITY
 func ~== (lhs: matrix, rhs: matrix) -> Bool{
     return (rhs.flat ~== lhs.flat)}
 
-operator infix == {associativity none precedence 140}
+infix operator == {associativity none precedence 140}
 func == (lhs: matrix, rhs: matrix)->matrix{
     return (lhs.flat == rhs.flat).reshape(lhs.shape)
 }
-operator infix !== {associativity none precedence 140}
+infix operator !== {associativity none precedence 140}
 func !== (lhs: matrix, rhs: matrix)->matrix{
     return (lhs.flat !== rhs.flat).reshape(lhs.shape)
 }
 
 /// ELEMENT WISE OPERATORS
 // PLUS
-operator infix + {associativity none precedence 140}
+infix operator + {associativity none precedence 140}
 func + (lhs: matrix, rhs: matrix) -> matrix{
     return make_operator(lhs, "+", rhs)}
 func + (lhs: Double, rhs: matrix) -> matrix{
@@ -92,7 +92,7 @@ func + (lhs: Double, rhs: matrix) -> matrix{
 func + (lhs: matrix, rhs: Double) -> matrix{
     return make_operator(lhs, "+", rhs)}
 // MINUS
-operator infix - {associativity none precedence 140}
+infix operator - {associativity none precedence 140}
 func - (lhs: matrix, rhs: matrix) -> matrix{
     return make_operator(lhs, "-", rhs)}
 func - (lhs: Double, rhs: matrix) -> matrix{
@@ -100,7 +100,7 @@ func - (lhs: Double, rhs: matrix) -> matrix{
 func - (lhs: matrix, rhs: Double) -> matrix{
     return make_operator(lhs, "-", rhs)}
 // TIMES
-operator infix * {associativity none precedence 140}
+infix operator * {associativity none precedence 140}
 func * (lhs: matrix, rhs: matrix) -> matrix{
     return make_operator(lhs, "*", rhs)}
 func * (lhs: Double, rhs: matrix) -> matrix{
@@ -108,7 +108,7 @@ func * (lhs: Double, rhs: matrix) -> matrix{
 func * (lhs: matrix, rhs: Double) -> matrix{
     return make_operator(lhs, "*", rhs)}
 // DIVIDE
-operator infix / {associativity none precedence 140}
+infix operator / {associativity none precedence 140}
 func / (lhs: matrix, rhs: matrix) -> matrix{
     return make_operator(lhs, "/", rhs)
 }
@@ -117,7 +117,7 @@ func / (lhs: Double, rhs: matrix) -> matrix{
 func / (lhs: matrix, rhs: Double) -> matrix{
     return make_operator(lhs, "/", rhs)}
 // LESS THAN
-operator infix < {associativity none precedence 140}
+infix operator < {associativity none precedence 140}
 func < (lhs: matrix, rhs: Double) -> matrix{
     return make_operator(lhs, "<", rhs)}
 func < (lhs: matrix, rhs: matrix) -> matrix{
@@ -125,7 +125,7 @@ func < (lhs: matrix, rhs: matrix) -> matrix{
 func < (lhs: Double, rhs: matrix) -> matrix{
     return make_operator(lhs, "<", rhs)}
 // GREATER THAN
-operator infix > {associativity none precedence 140}
+infix operator > {associativity none precedence 140}
 func > (lhs: matrix, rhs: Double) -> matrix{
     return make_operator(lhs, ">", rhs)}
 func > (lhs: matrix, rhs: matrix) -> matrix{
@@ -133,7 +133,7 @@ func > (lhs: matrix, rhs: matrix) -> matrix{
 func > (lhs: Double, rhs: matrix) -> matrix{
     return make_operator(lhs, ">", rhs)}
 // GREATER THAN OR EQUAL
-operator infix >= {associativity none precedence 140}
+infix operator >= {associativity none precedence 140}
 func >= (lhs: matrix, rhs: Double) -> matrix{
     return make_operator(lhs, "=>", rhs)}
 func >= (lhs: matrix, rhs: matrix) -> matrix{
@@ -141,7 +141,7 @@ func >= (lhs: matrix, rhs: matrix) -> matrix{
 func >= (lhs: Double, rhs: matrix) -> matrix{
     return make_operator(lhs, "=>", rhs)}
 // LESS THAN OR EQUAL
-operator infix <= {associativity none precedence 140}
+infix operator <= {associativity none precedence 140}
 func <= (lhs: matrix, rhs: Double) -> matrix{
     return make_operator(lhs, "=>", rhs)}
 func <= (lhs: matrix, rhs: matrix) -> matrix{
