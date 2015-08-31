0fa894959406fd706f94372e6ee7da72275b878b
scottsievert <sieve121@umn.edu>
2015-08-31 18:41:44 -0500

    removing find (not similar to Python)

--- swix/swix/swix/ndarray/helper-functions.swift
+++ swix/swix/swix/ndarray/helper-functions.swift
@@ -140,9 +140,6 @@ func argwhere(idx: ndarray) -> ndarray{
     vDSP_vcmprsD(!i, 1.stride, !idx, 1.stride, !args, 1.stride, idx.n.length)
     return args
 }
-func find(idx:ndarray, x:ndarray)->ndarray{
-    return x[idx]
-}
 
 
 // LOGICAL
